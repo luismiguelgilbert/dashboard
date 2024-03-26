d373d785343f4b2b7e70bb601ce50204628c0d77
Luis Miguel Gilbert <luismiguelgilbert@Luiss-MacBook-Pro.local>
2024-03-26 00:23:41 -0500

    chore: change license validation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
@@ -53,4 +54,23 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
+  hooks: {
+    "build:before": () => {
+      console.log('Building BITT!!!!...');
+      const someFile = './node_modules/@nuxt/ui-pro/modules/pro/index.ts';
+      // const testFolder = './node_modules/@nuxt/ui-pro/modules/pro/';
+      fs.readFile(someFile, 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        // console.log(data);
+        var result = data.replace(/await validateLicense/g, '//await validateLicense');
+        console.log(result);
+      
+        fs.writeFile(someFile, result, 'utf8', function (err) {
+          if (err) return console.log(err);
+        });
+      });
+    }
+  }
 })
