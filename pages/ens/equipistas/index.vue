c0bb65eb866e55c51aed06ff927957f343e2f6b5
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2024-06-02 02:01:44 -0500

    feat: fixes on Ens v9

--- pages/ens/equipistas/index.vue
+++ pages/ens/equipistas/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { actions, module, title } from './components/config';
-import { filter_options, teams_sort_options, type type_ens_members } from '@/types/server/ens_types';
+import { filter_options, type type_ens_members } from '@/types/server/ens_types';
 import indexTable from './components/indexTable.vue';
 import indexList from './components/indexList.vue';
 
@@ -29,8 +29,7 @@ const totalRows = computed(() => data.value?.[0]?.row_count ?? 0 );
               v-model:sortBy="state.filterPayload.sortBy"
               class="hidden sm:flex items-stretch flex-shrink-0 gap-1.5"
               :placeholder="`Buscar ${module}...`"
-              :filter-options="filter_options"
-              :sort-options="teams_sort_options" />
+              :filter-options="filter_options" />
           </div>
           <IndexCreateButton
             :actions="validatePermissions(actions, sessionData.userMenuData)"
@@ -45,8 +44,7 @@ const totalRows = computed(() => data.value?.[0]?.row_count ?? 0 );
           v-model:sortBy="state.filterPayload.sortBy"
           class="hidden sm:flex items-stretch flex-shrink-0 gap-1.5"
           :placeholder="`Buscar ${module}...`"
-          :filter-options="filter_options"
-          :sort-options="teams_sort_options" />
+          :filter-options="filter_options" />
       </UDashboardToolbar>
       <UProgress
         class="block sm:hidden"
