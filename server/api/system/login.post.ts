48fcb6add57ffbcf736e90dbfef4558a569db3e4
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2024-07-16 01:22:38 -0500

    feat: fixes on Users v38

--- server/api/system/login.post.ts
+++ server/api/system/login.post.ts
@@ -2,12 +2,18 @@ import supabaseClient from '@/server/utils/supabaseSession';
 import { login_credentials } from '@/types/server/system/session';
 
 export default defineEventHandler(async(event) => {
+  console.log('login 01');
+  console.log(supabaseClient);
   event.context.params = useSanitizeParams(event.context.params);
   const body = await readBody(event);
+  console.log('login 02');
   const payload = await login_credentials.cast(body);
+  console.log('login 03');
   login_credentials.validate(body, { strict: false, abortEarly: false });
-
+  console.log('login 04');
+  
   const { data, error } = await supabaseClient.auth.signInWithPassword(payload);
+  console.log('login 05');
   if (error) {
     throw createError({ statusCode: 400, statusMessage: error.message });
   }
