d598a68dcfe3ad3c0790bf611677b27a14467fc1
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2024-07-26 23:57:15 -0500

    feat: fixes on ENS_3

--- pages/ens/servicios/components/indexList.vue
+++ pages/ens/servicios/components/indexList.vue
@@ -31,20 +31,22 @@ const routerParamId = computed(() => route.params.id);
           ? 'p-4 text-sm cursor-pointer border-l-2 border-primary-500 dark:border-primary-400 bg-primary-100 dark:bg-primary-900/25'
           : 'p-4 text-sm cursor-pointer border-l-2 border-white dark:border-gray-900 hover:border-primary-500/25 dark:hover:border-primary-400/25 hover:bg-primary-100/50 dark:hover:bg-primary-900/10'">
         <div class="flex items-center justify-between">
-          <div class="font-semibold max-w-52">
-            {{ row.name_es }}
+          <div class="flex min-w-0 items-center gap-3">
+            <UAvatar
+              :alt="row.name_es[0]"
+              size="lg" />
+            <div class="min-w-0 flex-auto text-base font-semibold">
+              <p class="dark:text-white text-black truncate text-ellipsis">
+                {{ row.name_es }}
+              </p>
+              <p class="text-gray-500 italic dark:text-gray-400 truncate text-ellipsis">
+                Equipistas {{ row.user_count }}
+              </p>
+            </div>
           </div>
-          <UBadge
-            :label="row.is_active? 'activo' : 'inactivo'"
-            :color="row.is_active ? 'primary' : 'red'"
-            variant="subtle"
-            class="capitalize"
-          />
-        </div>
-        <div class="flex items-center justify-between">
-          <div>
-            Equipistas: {{ row.user_count }}
-          </div>
+          <UToggle
+            size="md"
+            :model-value="row.is_active" />
         </div>
       </div>
     </div>
